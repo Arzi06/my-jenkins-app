97b316ec976e547e55b9a48f666657c5222f3e56
Arzi06 <shibilovbashir@gmail.com>
2021-07-16 18:00:19 -0400

    production

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ "$BRANCH_NAME" = "production" ]
+if [ "$BRANCH_NAME" = "prod" ]
 then
     stagevar=prod
 elif [ "$BRANCH_NAME" = "main" ]
 then
-    stagevar=staging
+    stagevar=main
 elif [ $BRANCH_NAME = "dev" ]
 then
     stagevar=dev
@@ -32,16 +32,19 @@ then
     make push stage=$stagevar
 fi
 
-# deploy image regardless of stage
-# cd back/
-# make deploy stage=$stagevar
-# cd ..
-# cd data/
-# make deploy stage=$stagevar
-# cd .. 
-# cd data-script/
-# make deploy stage=$stagevar
-# cd ..
-# cd front/
-# make deploy stage=$stagevar
-# cd .. 
+deploy image regardless of stage
+cd pv-pvc
+make run stage=$stagevar
+cd ..
+cd back/
+make deploy stage=$stagevar
+cd ..
+cd data/
+make deploy stage=$stagevar
+cd .. 
+cd data-script/
+make deploy stage=$stagevar
+cd ..
+cd front/
+make deploy stage=$stagevar
+cd .. 
